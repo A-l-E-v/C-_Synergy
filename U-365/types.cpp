76b028f6539a89cc1c802d49b8151c639daaf1e9
Al Ev <alexey_e@mail.ru>
2024-10-29 07:42:15 +0300

    types are done!

--- U-365/types.cpp
+++ U-365/types.cpp
@@ -4,3 +4,37 @@
 // string. То есть, например, программа просит пользователя “Enter int: “, затем “Enter
 // double: “ и так далее, а в конце выводит “You entered: 5 3.14 random_string “ и так
 // далее.
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int int_x = 0;
+    double double_x = 0;
+    char char_x = ' ';
+    bool bool_x = true;
+    string string_x = "";
+
+    cout << "---Переменные различных типов ---\n\n";
+    cout << "Введите переменную типа int: ";
+    cin >> int_x;
+
+    cout << "Введите переменную типа double: ";
+    cin >> double_x;
+
+    cout << "Введите переменную типа char: ";
+    cin >> char_x;
+
+    cout << "Введите переменную типа bool (0|1): ";
+    cin >> bool_x;
+
+    cout << "Введите переменную типа string: ";
+    cin >> string_x;
+    cout << "\n";
+
+    cout << "Вы ввели: " << int_x << " " << double_x << " " << char_x << " " << bool_x << " " << string_x << "\n";
+
+    return 0;
+}
