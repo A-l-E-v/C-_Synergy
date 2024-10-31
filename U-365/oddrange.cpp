02f8a93da91dfa2c852c407a00798c9b1846d622
Al Ev <alexey_e@mail.ru>
2024-10-31 05:20:03 +0300

    oddrange done!

--- U-365/oddrange.cpp
+++ U-365/oddrange.cpp
@@ -7,6 +7,31 @@ using namespace std;
 
 int main()
 {
+    int a, b, x, y, ndx;
+    cout << "--- Нечётные числа диапазона ---\n\n";
+    cout << "Введите первую границу диапазона: ";
+    cin >> x;
+    cout << "Введите вторую границу диапазона: ";
+    cin >> y;
 
-    
+    // Определяем диапазон слева направо
+    if (y >= x)
+    {
+        a = x;
+        b = y;
+    }
+    else
+    {
+        a = y;
+        b = x;
+    }
+
+    for (int ndx = a; ndx <= b; ndx++)
+    {
+        if (ndx % 2 != 0)
+        {
+            cout << ndx << " ";
+        };
+    }
+    cout << "\n";
 }
