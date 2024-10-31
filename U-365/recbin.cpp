4f06d0b59eb852a19f34e7230c40ab875a29a944
Al Ev <alexey_e@mail.ru>
2024-10-31 06:01:15 +0300

    recbin&docx are done!

--- U-365/recbin.cpp
+++ U-365/recbin.cpp
@@ -7,8 +7,39 @@
 
 using namespace std;
 
-int main()
-{
+string bin = "";
 int x;
 
+void convert(int x, string &bin)
+{
+
+    if (x <= 1)
+    {
+        bin += (char)(x + '0');
+    }
+    else
+    {
+        convert(x / 2, bin);
+        if (x % 2)
+        {
+            bin += '1';
+        }
+        else
+        {
+            bin += '0';
+        }
+    }
+}
+
+int main()
+{
+    cout << "--- Перевод числа из десятичного в двоичное ---\n\n";
+    cout << "Введите число: ";
+    cin >> x;
+
+    convert(x, bin);
+    cout << "Число " << x << " в двоичной системе: ";
+    cout << bin << endl;
+
+    return 0;
 }
