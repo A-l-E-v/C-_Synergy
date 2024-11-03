bb5a72240f6e30769487d35e591690289cb2c11d
Al Ev <alexey_e@mail.ru>
2024-11-03 20:13:15 +0300

    task rectangle

---
+++ U-382/rect.cpp
@@ -0,0 +1,11 @@
+// Задание No1: Создайте класс Rectangle с двумя закрытыми полями width и height, а
+// также методами для вычисления площади и периметра прямоугольника.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
